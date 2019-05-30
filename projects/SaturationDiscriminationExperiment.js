6bcd75294cb95a902f394bc782df68fb182400c8
Sara Patterson <sarap44@uw.edu>
2019-05-29 19:05:21 -0700

    added split field and discrimination experiments

---
+++ projects/SaturationDiscriminationExperiment.js
@@ -0,0 +1,129 @@
+// SaturationDiscriminationExperiment.js
+// 
+// Increment test light until just noticable against white
+//
+// 29May2019 - SSP
+
+var backgroundIntensity;
+var circleIntensity;
+var circleDiameter;
+
+var lastBackgroundIntensity;
+var lastCircleIntensity;
+var firstFrame
+
+// User interface
+var bkgdSlider;
+var circleSlider;
+var fixationCheckbox;
+var fixationEnabled;
+
+// View intensity settings
+var verbose;
+
+function setup() {
+    backgroundIntensity = 255;
+    circleIntensity = 255;
+
+    circleDiameter = 80;
+    
+    firstFrame = true;
+    fixationEnabled = true;
+    verbose = true;
+    
+    createCanvas(windowWidth, windowHeight);
+
+    colorMode(RGB, 255, 255, 255, 100);
+    background(0, 0, backgroundIntensity);
+    noStroke();
+    ellipseMode(RADIUS);
+    frameRate(20);
+
+    // Slider to change the background intensity
+    bkgdSlider = createSlider(0, backgroundIntensity, 255, 5);
+    bkgdSlider.position(20, 20);
+
+    // Slider to change the circle intensity
+    circleSlider = createSlider(0, circleIntensity, 255, 1);
+    circleSlider.position(20, 50);
+
+    // Checkbox to toggle the fixation point
+    fill(255, 255, 255);
+    fixCheckbox = createCheckbox(' ', fixationEnabled);
+    fixCheckbox.position(20, 90);
+    fixCheckbox.changed(myCheckedEvent);
+}
+
+function draw() {    
+    if (firstFrame == true) {
+        // Always draw the scene on the first frame
+        drawScene();
+        firstFrame = false;
+    } else {
+        // Update intensity to match slider value
+        lastBackgroundIntensity = backgroundIntensity;
+        backgroundIntensity = bkgdSlider.value();
+        lastCircleIntensity = circleIntensity;
+        circleIntensity = circleSlider.value();
+
+        // Redraw the scene if intensity values changed
+        if (lastBackgroundIntensity != backgroundIntensity) {
+            drawScene();
+        } else if (lastCircleIntensity != circleIntensity) {
+            drawScene();
+        }
+    }
+}
+
+function drawScene() {
+    clear();
+    colorMode(RGB, 255, 255, 255, 100);
+    background(0, 0, backgroundIntensity);
+    noStroke();
+    ellipseMode(RADIUS);
+
+    fill(0, circleIntensity, backgroundIntensity);
+    ellipse(windowWidth/2, windowHeight/2, circleDiameter, circleDiameter);
+
+    addFixation();
+
+    if (verbose) {
+        textSize(14);
+        fill(255, 255, 255);
+        text("Circle", 10, 130);
+        text(circleIntensity, 150, 130);
+        text("Background", 10, 150);
+        text(backgroundIntensity, 150, 150);
+        text("Show Fixation", 50, 105);
+    }
+}
+
+function addFixation() {
+    if (fixationEnabled == true) {
+        fill(100, 100, 100);
+        ellipse(windowWidth / 2, windowHeight / 2, 2, 2);
+        fill(0, 0, 0);
+        ellipse(windowWidth / 2, windowHeight / 2, 1, 1);
+    }
+}
+
+function myCheckedEvent() {
+    if (this.checked()) {
+        fixationEnabled = true;
+        drawScene();
+    } else {
+        fixationEnabled = false;
+        drawScene();
+    }
+}
+
+function windowResized() {
+    if (fullscreen()) {
+        resizeCanvas(displayWidth, displayHeight);
+    } else {
+        resizeCanvas(windowWidth, windowHeight);
+    }
+    cursor();
+    showing = true;
+    drawScene();
+}
